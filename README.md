c55b4a76e074c61398fd1ae6b347e4219dfa8c0d
Chris Baltzer <cbaltzer@users.noreply.github.com>
2021-11-26 12:51:17 -0400

    CIImage conversion

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # QOI
 
-A description of this package.
+A thin Swift wrapper of `qoi` the [Quite OK Image format](https://github.com/phoboslab/qoi). 
+
+Still very much a work in progress, contributions are welcome.
+
+Supported functionality:
+- [x] Render for display
+- [x] Read from file
+- [ ] Decode in memory
+- [ ] Encode in memory
+- [ ] Write to file  
+
+
+## Usage
+
+```swift
+guard let url = Bundle.main.url(forResource: "test_output", withExtension: "qoi") else {
+    return
+}
+
+// Creates a CIImage which is available cross platform
+let ci = QOI.read(url: url) 
+
+// iOS  
+let image = UIImage(ciImage: ci)
+
+// macOS 
+if let cg = ci.cgImage {
+    let image = NSImage(cgImage: cg, size: .zero)
+}
+```
--- Sources/QOI/QOI.swift
+++ Sources/QOI/QOI.swift
@@ -1,15 +1,55 @@
 import Foundation
+import CoreImage
 import Cqoi
 
 public struct QOI {
 
+    /**
+     Reads a QOI file at the specified file URL and converts it into a CIImage for display.
+     Uses the devices default RGB colorspace, and assumes the image has 4 channels.
+
+     Example usage:
+     ```
+     let ci = QOI.read(url: url)
+     let image = UIImage(ciImage: ci)
+     ```
+
+        - parameters:
+            - url: A file URL pointing to a QOI file on disk
+     */
+    public static func read(url: URL) -> CIImage {
+        guard url.isFileURL else {
+            return CIImage()
+        }
+
+        let filename = url.path
+        var w: Int32 = 0
+        var h: Int32 = 0
+
+        let pixels = QOI.read(filename: filename, w: &w, h: &h, channels: 4)
+
+        let byesPerRow = Int(w * 4)
+        let size = CGSize(width: Int(w), height: Int(h))
+
+        let ci = CIImage(bitmapData: pixels,
+                         bytesPerRow: byesPerRow,
+                         size: size,
+                         format: .RGBA8,
+                         colorSpace: CGColorSpaceCreateDeviceRGB())
+
+        return ci
+    }
+
+
+    /**
+     Direct mapping of `qoi_read()`. You probably want `read(url:)` instead.
+     */
     public static func read(filename: String, w: inout Int32, h: inout Int32, channels: Int32) -> Data {
         guard let pixels = Cqoi.qoi_read(filename.cString(using: .utf8), &w, &h, channels) else {
-            print("no file")
             return Data()
         }
 
-        let data = Data(bytes: pixels, count: Int(w * h))
+        let data = Data(bytes: pixels, count: Int(w * h * (channels + 1) + Cqoi.QOI_HEADER_SIZE + Cqoi.QOI_PADDING))
 
         return data
     }
